799e99cddd2730130d79d1c3421f4f81cc61d390
Taylor Denouden <taylordenouden@gmail.com>
2021-02-22 15:17:32 -0800

    Refactoring

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from rasterio.transform import Affine
 
 def main(img_path: str, tile_index: str, out_dir: str):
     """
-    Crop the image at img_path to multiple small images using bounds defined by the polygons in tile_index and write
-        the resulting images to the out_dir.
+    Crop the image at img_path to multiple small images using bounds defined by the polygon bounding boxes in tile_index
+        and writes the resulting images to the out_dir.
 
     :param img_path:
         The path to the image to crop.
@@ -22,7 +22,7 @@ def main(img_path: str, tile_index: str, out_dir: str):
         None
     """
     # Open the image
-    imf = rasterio.open(img_path)
+    img = rasterio.open(img_path)
 
     # Open the shapefile
     sf = shapefile.Reader(tile_index)
@@ -33,11 +33,11 @@ def main(img_path: str, tile_index: str, out_dir: str):
     outpath = OutPath(out_dir, img_path)
 
     # Create the cropped images
-    img_saved = [crop_img_to_shp(imf, shape, outpath) for shape in sf.shapes()]
+    img_saved = [crop_img_to_shp(img, shape, outpath) for shape in sf.shapes()]
     print(f"{sum(img_saved)} images created")
 
     sf.close()
-    imf.close()
+    img.close()
 
 
 class OutPath(object):
@@ -80,36 +80,36 @@ class BBox(object):
         return rasterio.windows.Window(lt[1], lt[0], rb[0] - lt[0], rb[1] - lt[1])
 
 
-def crop_img_to_shp(imf: rasterio.DatasetReader, shape: shapefile.Shape, out_path: OutPath) -> bool:
+def crop_img_to_shp(img: rasterio.DatasetReader, shape: shapefile.Shape, out_path: OutPath) -> bool:
     # Get the bbox to crop to
     shp_bbox = BBox(*[round(v) for v in shape.bbox])
-    img_bbox = BBox(*list(imf.bounds))
+    img_bbox = BBox(*list(img.bounds))
     bbox = shp_bbox.intersect(img_bbox)
 
     if not bbox.is_valid:
         return False
 
     # Crop the image
-    window = bbox.to_window(imf)
-    data = imf.read(window=window)
+    window = bbox.to_window(img)
+    data = img.read(window=window)
 
     # Write to the output directory
     out = out_path.crop_path(shp_bbox.left, shp_bbox.bottom)
-    transform = Affine.translation(bbox.left, bbox.top) * Affine.scale(imf.transform.a, imf.transform.e)
+    transform = Affine.translation(bbox.left, bbox.top) * Affine.scale(img.transform.a, img.transform.e)
 
     with rasterio.open(out, 'w',
-                       driver=imf.driver,
+                       driver=img.driver,
                        height=window.height,
                        width=window.width,
-                       count=imf.count,
-                       dtype=imf.dtypes[0],
-                       crs=imf.crs,
+                       count=img.count,
+                       dtype=img.dtypes[0],
+                       crs=img.crs,
                        transform=transform,
-                       nodata=imf.nodata,
-                       mask_flag_enums=imf.mask_flag_enums,
+                       nodata=img.nodata,
+                       mask_flag_enums=img.mask_flag_enums,
                        ) as writer:
         writer.write(data)
-        writer.colorinterp = imf.colorinterp
+        writer.colorinterp = img.colorinterp
 
     print(f'Created: {out}')
     return True
