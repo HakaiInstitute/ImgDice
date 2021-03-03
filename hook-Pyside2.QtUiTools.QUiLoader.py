c641c93a488944fe24d28e105e3a400ba9a7cacf
Taylor Denouden <taylordenouden@gmail.com>
2021-03-03 14:12:36 -0800

    Add PyInstaller hooks

---
+++ hook-Pyside2.QtUiTools.QUiLoader.py
@@ -0,0 +1,7 @@
+hiddenimports = [
+    "PySide2.QtXml",
+]
+
+datas = [
+    ("ImgDice/form.ui", "form.ui")
+]
---
+++ hook-rasterio.py
@@ -0,0 +1,8 @@
+hiddenimports = [
+    "rasterio._shim",
+    "rasterio.control",
+    "rasterio.crs",
+    "rasterio.sample",
+    "rasterio.vrt",
+    "rasterio._features"
+]
